68de8b78ff63eac9a3a53064a9433f9ed36cb776
Noelma Carme Gondim de Oliveira <noelmagondim@gmail.com>
2022-04-06 14:32:03 -0300

    Desafio 08

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { expense } = this.props;
     return (
       <table>
         <tr>
@@ -15,9 +18,41 @@ class Table extends Component {
           <th>Moeda de conversão</th>
           <th>Editar/Excluir</th>
         </tr>
+        {expense.map((expenses) => {
+          const {
+            id,
+            description,
+            tag,
+            method,
+            value,
+            exchangeRates,
+            currency } = expenses;
+          return (
+            <tr key={ id }>
+              <td>{ description }</td>
+              <td>{ tag }</td>
+              <td>{ method }</td>
+              <td>{ Number(value).toFixed(2) }</td>
+              <td>{ exchangeRates[currency].name }</td>
+              <td>{ Number(exchangeRates[currency].ask).toFixed(2) }</td>
+              <td>
+                { Number(exchangeRates[currency].ask) * Number(value) }
+              </td>
+              <td>Real</td>
+            </tr>
+          );
+        })}
       </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expense: PropTypes.arrayOf(PropTypes.any).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expense: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps, null)(Table);
